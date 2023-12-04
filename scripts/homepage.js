ed1174a681d9a96f5a65c93a4cad2ce0f5b3fbb9
Ekrem Taha SENER <robotekrem55@gmail.com>
2023-12-04 21:36:14 +0100

    -Updated homepage -Changed font to Comfortaa -Added page for RPP

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -1,47 +1,49 @@
-const scrollButton = document.getElementById('scrollButton');
-scrollButton.addEventListener('click', scrollDown);
+document.addEventListener("DOMContentLoaded", () => {
+    const scrollButton = document.getElementById('scrollButton');
+    scrollButton.addEventListener('click', scrollDown);
 
-const imAddram = document.querySelector('#imaddram');
-const originalImAddram = imAddram.textContent;
+    const imAddram = document.querySelector('#imaddram');
+    const originalImAddram = imAddram.textContent;
 
-function scrollDown() {
-    const scrollAmount = 0.95;
-    const targetScroll = scrollAmount * window.innerHeight;
-    window.scrollTo({
-        top: targetScroll,
-        behavior: 'smooth'
-    });
-}
-
-imAddram.addEventListener('mouseenter', (e) => {
-    slowlyRandomizeAddram();
-});
-
-document.addEventListener("DOMContentLoaded", (e) => {
-    slowlyRandomizeAddram();
-});
-
-async function slowlyRandomizeAddram(){
-    console.log("randomizing")
-    for(let i = imAddram.textContent.length; i >= 0; i--){
-        await new Promise(r => {
-            setTimeout(() => {
-                imAddram.textContent = randomify(originalImAddram, i);
-                r();
-            }, 60);
+    function scrollDown() {
+        const scrollAmount = 0.95;
+        const targetScroll = scrollAmount * window.innerHeight;
+        window.scrollTo({
+            top: targetScroll,
+            behavior: 'smooth'
         });
     }
-}
 
-function randomify(string, amount){
-    let newString = "";
-    let amtFromBeginning = string.length - amount;
-    const randomChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789?!£$%&";
-    for(let i = 0; i < string.length; i++){
-        if(i < amtFromBeginning) newString += string[i];
-        else{
-            newString += randomChars[Math.floor(Math.random() * randomChars.length)];
+    imAddram.addEventListener('mouseenter', (e) => {
+        slowlyRandomizeAddram();
+    });
+
+    document.addEventListener("DOMContentLoaded", (e) => {
+        slowlyRandomizeAddram();
+    });
+
+    async function slowlyRandomizeAddram() {
+        console.log("randomizing")
+        for (let i = imAddram.textContent.length; i >= 0; i--) {
+            await new Promise(r => {
+                setTimeout(() => {
+                    imAddram.textContent = randomify(originalImAddram, i);
+                    r();
+                }, 60);
+            });
         }
     }
-    return newString;
-}
+
+    function randomify(string, amount) {
+        let newString = "";
+        let amtFromBeginning = string.length - amount;
+        const randomChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789?!£$%&";
+        for (let i = 0; i < string.length; i++) {
+            if (i < amtFromBeginning) newString += string[i];
+            else {
+                newString += randomChars[Math.floor(Math.random() * randomChars.length)];
+            }
+        }
+        return newString;
+    }
+});
--- scripts/transitioningeffect.js
+++ scripts/transitioningeffect.js
@@ -1,53 +1,55 @@
-let elementsArray = document.querySelectorAll(".totransition");
-window.addEventListener('scroll', fadeIn ); 
-function fadeIn() {
-    for (var i = 0; i < elementsArray.length; i++) {
-        var elem = elementsArray[i]
-        var distInView = elem.getBoundingClientRect().top - window.innerHeight + 20;
-        if (distInView < 0) {
-            elem.classList.add("inView");
-            if((
-                elem.classList.contains('specialfont') ||
-                elem.classList.contains('h1-desc') ||
-                elem.classList.contains('blue-bigger-hovereffect') ||
-                // check if elem is an h2
-                elem.tagName.toLowerCase() == 'h2'
-                ) && !elem.id.includes('imaddram')){
-                slowlyRandomize(elem);
+document.addEventListener("DOMContentLoaded", () => {
+    let elementsArray = document.querySelectorAll(".totransition");
+    window.addEventListener('scroll', fadeIn);
+    function fadeIn() {
+        for (var i = 0; i < elementsArray.length; i++) {
+            var elem = elementsArray[i]
+            var distInView = elem.getBoundingClientRect().top - window.innerHeight + 20;
+            if (distInView < 0) {
+                elem.classList.add("inView");
+                if ((
+                    elem.classList.contains('specialfont') ||
+                    elem.classList.contains('h1-desc') ||
+                    elem.classList.contains('blue-bigger-hovereffect') ||
+                    // check if elem is an h2
+                    elem.tagName.toLowerCase() == 'h2'
+                ) && !elem.id.includes('imaddram')) {
+                    slowlyRandomize(elem);
+                }
+            } else {
+                elem.classList.remove("inView");
             }
-        } else {
-            elem.classList.remove("inView");
         }
     }
-}
-fadeIn();
+    fadeIn();
 
-async function slowlyRandomize(element){
-    if(element == undefined) return;
-    if(element.dataset.hasOwnProperty("randomized") && element.dataset.randomized == 'true') return;
-    let og = element.textContent;
-    element.dataset.original = og;
-    element.dataset.randomized = true;
-    for(let i = element.textContent.length; i >= 0; i--){
-        await new Promise(r => {
-            setTimeout(() => {
-                element.textContent = randomify(og, i);
-                r();
-            }, 40);
-        });
-    }
-}
-
-function randomify(string, amount){
-    let newString = "";
-    let amtFromBeginning = string.length - amount;
-    const randomChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789?!£$%&";
-    for(let i = 0; i < string.length; i++){
-        if(i < amtFromBeginning) newString += string[i];
-        else if(string[i] == ' ') newString += ' ';
-        else{
-            newString += randomChars[Math.floor(Math.random() * randomChars.length)];
+    async function slowlyRandomize(element) {
+        if (element == undefined) return;
+        if (element.dataset.hasOwnProperty("randomized") && element.dataset.randomized == 'true') return;
+        let og = element.textContent;
+        element.dataset.original = og;
+        element.dataset.randomized = true;
+        for (let i = element.textContent.length; i >= 0; i--) {
+            await new Promise(r => {
+                setTimeout(() => {
+                    element.textContent = randomify(og, i);
+                    r();
+                }, 40);
+            });
         }
     }
-    return newString;
-}
+
+    function randomify(string, amount) {
+        let newString = "";
+        let amtFromBeginning = string.length - amount;
+        const randomChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789?!£$%&";
+        for (let i = 0; i < string.length; i++) {
+            if (i < amtFromBeginning) newString += string[i];
+            else if (string[i] == ' ') newString += ' ';
+            else {
+                newString += randomChars[Math.floor(Math.random() * randomChars.length)];
+            }
+        }
+        return newString;
+    }
+});
