23ae48eb6573824953a05a0f7578d692f45409b8
Ekrem Taha SENER <robotekrem55@gmail.com>
2024-01-05 21:35:30 +0100

    Changed the way projects tab works. Added dynamic project loading from portfolio.json

---
+++ scripts/portfolio.js
@@ -0,0 +1,80 @@
+const tabs = [];
+
+$(document).ready(() => {
+    // tabs = $('.tab-content');
+    // const fccTab = $('#fcc-tab');
+    // switchTab(0, fccTab);
+    fetchPortfolioData().then(data => {populateTabs(data)});
+});
+
+
+function switchTab(tabIndex, clickedElement){
+    tabs.forEach(tab => {
+        $(tab).removeClass("active");
+    });
+    // show tab with index
+    $(tabs[tabIndex]).addClass("active");
+    if(clickedElement != null){
+        // remove active class from all tabs
+        $('.tab-switcher').removeClass('tab-active');
+        // add active class to clicked tab
+        $(clickedElement).addClass('tab-active');
+    }
+}
+
+async function fetchPortfolioData() {
+    try {
+        const response = await fetch('../projects/portfolio.json'); // Fetch the JSON file from the projects folder
+        if (!response.ok) {
+            throw new Error('Network response was not ok.');
+        }
+        const data = await response.json(); // Extract JSON from the response
+        return data; // Return the JSON data
+    } catch (error) {
+        console.error('There was a problem fetching the portfolio data:', error);
+        return null; // Return null or handle the error accordingly
+    }
+}
+
+
+function populateTabs(data){
+    const tabsContainer = $('#tabs-container');
+    const tabSwitcher = $('#tab-switchers');
+    tabsContainer.empty();
+    tabSwitcher.empty();
+    // for each property in data
+    // data structure: { fccTab: [], webTab: [], ... }
+    let keys = Object.keys(data);
+    for (let i = 0; i < keys.length; i++) {
+        let key = keys[i];
+        let value = data[key];
+        // create tabSwitcher 
+        const tabSwitcherElement = `<button class="tab-switcher" onclick="switchTab(${i}, this)" id="${key}Switcher">${value.title}</button>`;
+        tabSwitcher.append(tabSwitcherElement);
+        // create tab
+        const tabHTML = `
+        <div class="tab-content" id="${key}">
+            <h2 class="tab-title specialfont">${value.title}</h2>
+        </div>
+        `;
+        tabsContainer.append(tabHTML);
+        const tab = $(`#${key}`)
+        tabs.push(tab);
+        // create projects
+        const projects = value.children;
+        for (let j = 0; j < projects.length; j++) {
+            const project = projects[j];
+            const projectHTML = `
+            <div class="project totransition">
+                <div class="img-container">
+                    <img ${project.pixelated ? 'style="image-rendering: pixelated"' : ""} src="${project.imgSrc}" alt="${project.title} Icon" loading="lazy">
+                </div>
+                <p class="title totransition">${project.title}</p>
+            </div>
+            `;
+            tab.append(projectHTML);
+        }
+    }
+    switchTab(0, $('#fccTabSwitcher'));
+    initTransitions();
+}
--- scripts/transitioningeffect.js
+++ scripts/transitioningeffect.js
@@ -1,66 +1,73 @@
+let elementsArray;
+
 document.addEventListener("DOMContentLoaded", () => {
-    let elementsArray = document.querySelectorAll(".totransition");
-    window.addEventListener('scroll', fadeIn);
-    function fadeIn() {
-        for (var i = 0; i < elementsArray.length; i++) {
-            var elem = elementsArray[i]
-            var distInView = elem.getBoundingClientRect().top - window.innerHeight + 20;
-            if (distInView < 0) {
-                elem.classList.add("inView");
-
-                setTimeout(classRemover(elem, "totransition"), 400)
-
-                
-                if ((
-                    elem.classList.contains('specialfont') ||
-                    elem.classList.contains('h1-desc') ||
-                    elem.classList.contains('blue-bigger-hovereffect') ||
-                    // check if elem is an h2
-                    elem.tagName.toLowerCase() == 'h2'
-                ) && !elem.id.includes('imaddram')) {
-                    slowlyRandomize(elem);
-                }
-            } 
-            // else {
-            //     elem.classList.remove("inView");
-            // }
-        }
-    }
-    fadeIn();
-
-    async function slowlyRandomize(element) {
-        if (element == undefined) return;
-        if (element.dataset.hasOwnProperty("randomized") && element.dataset.randomized == 'true') return;
-        let og = element.textContent;
-        element.dataset.original = og;
-        element.dataset.randomized = true;
-        for (let i = element.textContent.length; i >= 0; i--) {
-            await new Promise(r => {
-                setTimeout(() => {
-                    element.textContent = randomify(og, i);
-                    r();
-                }, 40);
-            });
-        }
-    }
-
-    function randomify(string, amount) {
-        let newString = "";
-        let amtFromBeginning = string.length - amount;
-        const randomChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789?!£$%&";
-        for (let i = 0; i < string.length; i++) {
-            if (i < amtFromBeginning) newString += string[i];
-            else if (string[i] == ' ') newString += ' ';
-            else {
-                newString += randomChars[Math.floor(Math.random() * randomChars.length)];
-            }
-        }
-        return newString;
-    }
+    initTransitions();
 });
+
+function initTransitions(){
+    elementsArray = document.querySelectorAll(".totransition");
+    window.addEventListener('scroll', fadeIn);
+    fadeIn();
+}
 
 function classRemover(element, classToRemove){
     return function(){
         element.classList.remove(classToRemove);
+    }
+}
+
+function fadeIn() {
+    for (var i = 0; i < elementsArray.length; i++) {
+        var elem = elementsArray[i]
+        var distInView = elem.getBoundingClientRect().top - window.innerHeight + 20;
+        if (distInView < 0) {
+            elem.classList.add("inView");
+
+            setTimeout(classRemover(elem, "totransition"), 400)
+
+            
+            if ((
+                elem.classList.contains('specialfont') ||
+                elem.classList.contains('h1-desc') ||
+                elem.classList.contains('blue-bigger-hovereffect') ||
+                // check if elem is an h2
+                elem.tagName.toLowerCase() == 'h2'
+            ) && !elem.id.includes('imaddram')) {
+                slowlyRandomize(elem);
+            }
+        } 
+        // else {
+        //     elem.classList.remove("inView");
+        // }
+    }
+}
+
+function randomify(string, amount) {
+    let newString = "";
+    let amtFromBeginning = string.length - amount;
+    const randomChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789?!£$%&";
+    for (let i = 0; i < string.length; i++) {
+        if (i < amtFromBeginning) newString += string[i];
+        else if (string[i] == ' ') newString += ' ';
+        else {
+            newString += randomChars[Math.floor(Math.random() * randomChars.length)];
+        }
+    }
+    return newString;
+}
+
+async function slowlyRandomize(element) {
+    if (element == undefined) return;
+    if (element.dataset.hasOwnProperty("randomized") && element.dataset.randomized == 'true') return;
+    let og = element.textContent;
+    element.dataset.original = og;
+    element.dataset.randomized = true;
+    for (let i = element.textContent.length; i >= 0; i--) {
+        await new Promise(r => {
+            setTimeout(() => {
+                element.textContent = randomify(og, i);
+                r();
+            }, 40);
+        });
     }
 }
